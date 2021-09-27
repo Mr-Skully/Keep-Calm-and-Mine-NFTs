db0f1e5bd136babfa4fdfb0e4fcb963384fa83bc
Mr-Skully <emiltitus.inc@gmail.com>
2021-09-27 14:52:25 +0530

    Create dummy contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract NFT {
+    constructor() {
+        console.log("This is my NFT contract. Woah!");
+    }
+}
